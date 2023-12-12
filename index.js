9d0882b82cf2d2221875083fd39ec0c77525e358
Nestor Masinde <nestormasinde@gmail.com>
2023-12-12 18:05:13 +0300

    Arrow Function

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(){
+    let h = 2000;
+    let p = 100;
+    return h/p;
+}
+console.log(divide());
+
+
+const square = x => x * x;
+console.log(square(2));
+
+const add = (a, b) => {
+    const result = a + b;
+    return result;
+}
+
